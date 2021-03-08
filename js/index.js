454f6e287413fe357d8f86508f18b8a83af34031
JongMin, Kim <rlawhdals228@gmail.com>
2021-03-08 22:13:45 +0900

    캐러샐추가

---
+++ js/index.js
@@ -0,0 +1,36 @@
+/* game tab 버튼 */
+$(function () {
+    $('.js_btn_pcgame').on('click', function () {
+        $('.js_btn_mogame').removeClass('on');
+        $('.js_btn_pcgame').addClass('on');
+        $('.gnb_mogame_wrap').removeClass('on');
+        $('.gnb_pcgame_wrap').addClass('on');
+    });
+    $('.js_btn_mogame').on('click', function () {
+        $('.js_btn_pcgame').removeClass('on');
+        $('.js_btn_mogame').addClass('on');
+        $('.gnb_pcgame_wrap').removeClass('on');
+        $('.gnb_mogame_wrap').addClass('on');
+    });
+
+    $('.js_btn_gnb_open').on('click', function () {
+        $('header').addClass('open');
+    });
+
+    $('.gnb_dimmed').on('click', function () {
+        $('header').removeClass('open');
+    })
+    $('.js_btn_gnb_close').on('click', function () {
+        $('header').removeClass('open');
+    })
+});
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        // autoplay: true,
+        // autoplayTimeout: 1800,
+        // autoplayHoverPause: true
+    });
+});
