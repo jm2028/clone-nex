1bb739fa25208195eaf4da24e1dcc814f4ef834c
JongMin, Kim <rlawhdals228@gmail.com>
2021-03-24 01:18:56 +0900

    view type

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 /* game tab 버튼 */
 $(function () {
+    /* nav open */
+    $('.js_btn_gnb_open').on('click', function () {
+        $('header').addClass('open');
+    });
+    /* nav submenu 변경 */
     $('.js_btn_pcgame').on('click', function () {
         $('.js_btn_mogame').removeClass('on');
         $('.js_btn_pcgame').addClass('on');
@@ -12,17 +17,21 @@ $(function () {
         $('.gnb_pcgame_wrap').removeClass('on');
         $('.gnb_mogame_wrap').addClass('on');
     });
-
-    $('.js_btn_gnb_open').on('click', function () {
-        $('header').addClass('open');
-    });
-
+    /* dimmed 클릭시 menu 닫힘 */
     $('.gnb_dimmed').on('click', function () {
         $('header').removeClass('open');
-    })
+    });
     $('.js_btn_gnb_close').on('click', function () {
         $('header').removeClass('open');
-    })
+    });
+
+    /* view type */
+    $('.js_btn_thumbnail').on('click',function () {
+        $('.game_wrap').removeClass('type_list').addClass('type_thumbnail');
+    });
+    $('.js_btn_list').on('click',function () {
+        $('.game_wrap').removeClass('type_thumbnail').addClass('type_list');
+    });
 });
 
 $(document).ready(function(){
